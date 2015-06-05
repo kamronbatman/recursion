660f4b07f1dde85657952b2793c60c0f12c406c7
Kamron Batman <kamron.batman@gmail.com>
2015-06-04 23:04:23 -0700

    Completed the stringifyJSON recursion. Tested and it passes.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var objType = typeof obj;
+	console.log('Type: ' + objType + ' and ' + ((obj === null || objType === 'undefined') ? 'null' : obj.toString()) );
+	// your code goes here
+	if (obj === null) {
+		return 'null';
+	} else if ( objType === 'number' || objType === 'boolean' ) {
+		return obj.toString();
+	}
+	else if ( objType === 'string' ) {
+		return '"' + obj.toString() + '"';
+	}
+	else if (Array.isArray(obj)) {
+		return '[' +
+			_.chain(obj)
+			.filter(function(value){ var valType = typeof value; return valType !== 'function' && valType !== 'undefined'; })
+			.reduce(function(memo, value, index, list){
+				if (index > 0) { memo += ','; }
+				return memo += stringifyJSON(value); //Prettify?
+			}, '').value()
+			+ ']';
+	} else if (objType !== 'function') {
+		return '{' +
+			(_.chain(obj)
+			.reduce(function(memo, value, key, list){
+				var valType = typeof value;
+				if (valType !== 'function' && valType !== 'undefined') {
+					if (memo.length > 0) { memo += ','; }
+					return memo += stringifyJSON(key) + ':' + stringifyJSON(value);
+				}
+			}, '').value() || '')
+			+ '}';
+	}
 };
